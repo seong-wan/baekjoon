4bc6496b9562c6b2ccbb0c3d21a4cc7b6ed4c402
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-07-19 08:59:05 +0900

    [Bronze V] Title: 영수증, Time: 220 ms, Memory: 17964 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		int N = sc.nextInt();
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			sum += a * b; // 개수 가격 곱해서 합산
+
+		}
+		if (X == sum) {
+			System.out.println("Yes");
+
+		} else {
+			System.out.println("No");
+		}
+
+	}
+
+}
