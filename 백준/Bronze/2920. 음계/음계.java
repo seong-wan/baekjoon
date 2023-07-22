b26da8bf555425248cd602e821b51e7afcf448f6
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-07-22 14:13:26 +0900

    [Bronze II] Title: 음계, Time: 200 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -5,35 +5,35 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int[] Arr = new int[8];
-		int cnt = 0;
+		int chk = 0;
 
 		for (int i = 0; i < 8; i++) {
 			Arr[i] = sc.nextInt();
 
 		}
-		cnt = Arr[0];
-		if (cnt == 1) {
+		chk = Arr[0];
+		if (chk == 1) {
 			for (int i = 0; i < 8; i++) {
-				if (Arr[i] == cnt) {
-					if (cnt == 8) {
+				if (Arr[i] == chk) {
+					if (chk == 8) {
 						System.out.println("ascending");
 
 					}
-					cnt++;
+					chk++;
 				} else {
 					System.out.println("mixed");
 					break;
 				}
 
 			}
-		} else if (cnt == 8) {
+		} else if (chk == 8) {
 			for (int i = 0; i < 8; i++) {
-				if (Arr[i] == cnt) {
-					if (cnt == 1) {
+				if (Arr[i] == chk) {
+					if (chk == 1) {
 						System.out.println("descending");
 
 					}
-					cnt--;
+					chk--;
 
 				} else {
 					System.out.println("mixed");
