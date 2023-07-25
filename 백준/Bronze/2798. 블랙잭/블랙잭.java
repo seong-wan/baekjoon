7a5599c1aeb53fd7128a089bcb12c9885830f528
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-07-26 02:49:40 +0900

    [Bronze II] Title: 블랙잭, Time: 272 ms, Memory: 18040 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int max = 0;
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		ArrayList<Integer> arr = new ArrayList<>();
+
+		for (int i = 0; i < N; i++) {
+			arr.add(sc.nextInt());
+
+		}
+		for (int i = 0; i < arr.size() - 2; i++) {
+			for (int j = i + 1; j < arr.size() - 1; j++) {
+				for (int j2 = j + 1; j2 < arr.size(); j2++) {
+					if (arr.get(i) + arr.get(j) + arr.get(j2) <= M) {
+						if (arr.get(i) + arr.get(j) + arr.get(j2) > max) {
+							max = arr.get(i) + arr.get(j) + arr.get(j2);
+						}
+					}
+
+				}
+
+			}
+
+		}
+		System.out.println(max);
+	}
+
+}
