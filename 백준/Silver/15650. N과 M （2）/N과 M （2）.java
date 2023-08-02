4999e1b838661a956a62a0072e6167e0c58d4528
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-02 12:28:15 +0900

    [Silver III] Title: N과 M (2), Time: 156 ms, Memory: 15852 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,41 +1,42 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] src;
+	static int N, M;
 	static int[] tgt;
-	static StringBuilder sb = new StringBuilder();
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+	
 	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		src = new int[N];
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		tgt = new int[M];
-		for (int i = 0; i < N; i++) {
-			src[i] = i + 1;
-		}
-		comb(0, 0);
-		System.out.print(sb);
+		
+		comb(1,0);
+		
 	}
-
-	static void comb(int tgtIdx, int srcIdx) {
-		if (tgtIdx == tgt.length) {
+	
+	static void comb(int nIdx, int tgtIdx ) {
+		//기저조건
+		if(tgtIdx == M) {
 			for (int i : tgt) {
-
-				sb.append(i).append(" ");
+				System.out.print(i + " ");
 			}
-			sb.append("\n");
+			System.out.println();
 			return;
 		}
-		if (srcIdx == src.length)
-			return;
-		tgt[tgtIdx] = src[srcIdx];
-		comb(tgtIdx + 1, srcIdx + 1);
-		comb(tgtIdx, srcIdx + 1);
+		
+		if(nIdx == N+1) return;
+		
+			tgt[tgtIdx] = nIdx;
+			comb(nIdx+1, tgtIdx+1);
+			comb(nIdx+1, tgtIdx);
 
 	}
+
 }
