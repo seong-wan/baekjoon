05d4074a85cf5091f5224f89d99ba6d4f62614d5
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-03 17:01:08 +0900

    [Silver II] Title: DNA 비밀번호, Time: 312 ms, Memory: 21948 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,74 +1,85 @@
-//단 부분문자열이 등장하는 위치가 다르다면 부분문자열이 같다고 하더라도 다른 문자열로 취급한다.
-//부분 문자열의 의미 -> 문자를 하나하나 잘라서 넣는게 아니라 이어진 문자 그대로 넣어야함 
-//각각 들고오는게 xxxxxxxxx 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static char[] ch;
+	static StringTokenizer st;
+	static int cnt;
+	static int A, C, G, T, A_c, C_c, G_c, T_c;
 
-	static int S, P, ans;
-	static char[] dna; //String 대신 char 배열
-	static int minA, minC, minG, minT, cntA, cntC, cntG, cntT;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		S = Integer.parseInt(st.nextToken());
-		P = Integer.parseInt(st.nextToken());
-		
-		dna = br.readLine().toCharArray();
+	public static void main(String[] args) throws Exception, IOException {
 		st = new StringTokenizer(br.readLine());
-		minA = Integer.parseInt(st.nextToken());
-		minC = Integer.parseInt(st.nextToken());
-		minG = Integer.parseInt(st.nextToken());
-		minT = Integer.parseInt(st.nextToken());
-		
-		solve();
-		System.out.println(ans);
-	}
-	
-	static void solve() {
-		//전체 문자열 S의 맨 앞 P(부분문자열의 길이)개의 문자를 우선 계산
+		int S = Integer.parseInt(st.nextToken());
+		int P = Integer.parseInt(st.nextToken());
+		String code = br.readLine();
+		ch = code.toCharArray();
+		st = new StringTokenizer(br.readLine());
+		A = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		G = Integer.parseInt(st.nextToken());
+		T = Integer.parseInt(st.nextToken());
 		for (int i = 0; i < P; i++) {
-			switch(dna[i]) {
-			case 'A' : cntA++; break;
-			case 'C' : cntC++; break;
-			case 'G' : cntG++; break;
-			case 'T' : cntT++; break;
+			switch (ch[i]) {
+			case 'A':
+				A_c++;
+				break;
+			case 'C':
+				C_c++;
+				break;
+			case 'G':
+				G_c++;
+				break;
+			case 'T':
+				T_c++;
+				break;
 			}
+
 		}
-		
-		check();
-		//이후에는 P문자열 맨 앞 문자를 버리고, P문자열 맨 뒤 문자에 이어지는 문자를 얻는 과정을 반복 (끝까지 도달할 때 까지)
-		//함께 유효성 검증 => 통과하면 ans++ , check() 생성해서 사용
-		
+		if (A_c >= A && C_c >= C && G_c >= G && T_c >= T)
+			cnt++;
+//		A_c = str_arr[0].length() - str_arr[0].replace("A", "").length();
+//		C_c = str_arr[0].length() - str_arr[0].replace("C", "").length();
+//		G_c = str_arr[0].length() - str_arr[0].replace("G", "").length();
+//		T_c = str_arr[0].length() - str_arr[0].replace("T", "").length();
+//		if (A_c >= A && C_c >= C && G_c >= G && T_c >= T)
+//			cnt++;
+
 		for (int i = P; i < S; i++) {
-			//이전 부분문자열과 다른 새로운 부분 문자열을 완성
-			//이전 부분 문자열의 맨 앞 하나를 버리고 
-			switch(dna[i-P]) {
-			case 'A' : cntA--; break;
-			case 'C' : cntC--; break;
-			case 'G' : cntG--; break;
-			case 'T' : cntT--; break;
+			switch (ch[i]) {
+			case 'A':
+				A_c++;
+				break;
+			case 'C':
+				C_c++;
+				break;
+			case 'G':
+				G_c++;
+				break;
+			case 'T':
+				T_c++;
+				break;
 			}
-			//이전 부분 문자열의 맨뒤 다음것을 얻는다.
-			switch(dna[i]) {
-			case 'A' : cntA++; break;
-			case 'C' : cntC++; break;
-			case 'G' : cntG++; break;
-			case 'T' : cntT++; break;
+			switch (ch[i - P]) {
+			case 'A':
+				A_c--;
+				break;
+			case 'C':
+				C_c--;
+				break;
+			case 'G':
+				G_c--;
+				break;
+			case 'T':
+				T_c--;
+				break;
 			}
-			
-			check();
+			if (A_c >= A && C_c >= C && G_c >= G && T_c >= T)
+				cnt++;
 		}
+		System.out.println(cnt);
 	}
-	
-	//check() 가 호출될 때는 부분문자열 경우가 하나 완성된 상태
-	static void check() {
-		if(cntA >= minA && cntC >= minC && cntG >= minG && cntT >= minT ) ans++;
-	}
-		
+
 }
