05e41cd72da2ad68fb6aa2bb95629d4bff353e57
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-04 01:40:22 +0900

    [Silver III] Title: 미로 만들기, Time: 156 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Silver/1347. 미로 만들기/미로 만들기.java
+++ 백준/Silver/1347. 미로 만들기/미로 만들기.java
@@ -1,109 +1,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int num;
-	static char[] charArr;
-	static char road = '.';
-	static char wall = '#';
-	static char[][] map = new char[100][100];
-	//남 서 북 동  ++오른쪽 회전 , --왼쪽회전
-	static int[] dy = { 1, 0, -1, 0};
-	static int[] dx = { 0, -1, 0, 1};
-	static StringBuilder sb = new StringBuilder();
+	static char[][] map = new char[99][99]; // 최대 값으로 배열 선언
+	static int y = 49, x = 49, start_y = 49, end_y = 49, start_x = 49, end_x = 49; // map 중간에서 출발(앞으로 쭉 가도 맵 밖으로 안 감)
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int[] dy = { 1, 0, -1, 0 };
+	static int[] dx = { 0, -1, 0, 1 }; // 시계 방향(R입력 방향)
+	static int dir; // 방향 인덱스
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		num = Integer.parseInt(st.nextToken());
-		charArr = new char[num];
-		String str = br.readLine();
-
-		for(int i=0; i<num; i++) {
-			charArr[i] = str.charAt(i);
-		}
-		
-		int startY = 50;
-		int startX = 50;
-		
-		map[startY][startX] = road;
-		
-		maze(startY, startX); //지도 만들기
-		
-		printMaze(); //벽 채우기
-		System.out.println(sb);
-		
+	public static void main(String[] args) throws IOException {
+		search();// 입력에 따라 가는 길에 .찍기
+		print();// 미로 맵 범위 빈 칸에 #찍고 출력
 	}
-	
-	static void maze(int Y, int X) {
-		int nowDir = 0; //처음 방향은 남쪽
-		int ny = Y;
-		int nx = X;
-		
-		for(int i=0; i<num; i++) {
-			char dir = charArr[i];
 
-			if(dir == 'R') {
-				if(nowDir == 3) { //방향 바꾸기
-					nowDir = 0;
-				} else {
-					nowDir++;
-				}
-				
-			} else if(dir == 'L') {
-				if(nowDir == 0) { //방향 바꾸기
-					nowDir = 3;
-				} else {
-					nowDir--;
-				}
-				
-			} else { //F
-				ny += dy[nowDir];
-				nx += dx[nowDir];
-				
-				map[ny][nx] = road;
+	static void search() throws IOException {
+		map[y][x] = '.'; // 첫 칸에 .
+		int N = Integer.parseInt(br.readLine());
+		String command = br.readLine();
+		char cmd[] = command.toCharArray();
+		for (int i = 0; i < N; i++) {
+			switch (cmd[i]) {
+			case 'F':
+				y += dy[dir];
+				x += dx[dir];
+				map[y][x] = '.';
+				if (y < start_y)
+					start_y = y;
+				if (y > end_y)
+					end_y = y;
+				if (x < start_x)
+					start_x = x;
+				if (x > end_x)
+					end_x = x;
+				break; // F인 경우 가야하는 방향으로 이동 후 .찍고 맵 범위 확인을 위해 시작점과 끝 확인
+			case 'R':
+				dir++;
+				if (dir == 4)
+					dir = 0;
+				break; // R인 경우 방향 인덱스 +1 범위 넘어갈 시 3->0으로 이동
+			case 'L':
+				dir--;
+				if (dir == -1)
+					dir = 3;
+				break;// L인 경우 방향 인덱스 -1 범위 넘어갈 시 0->3으로 이동
+
 			}
+
 		}
-		
 	}
-	
-	static void printMaze() {
-		int minY = 99;
-		int minX = 99;
-		int maxY = 0;
-		int maxX = 0;
-		int tempX, tempY = 0;
-		
-		for(int i=0; i<100; i++) {
-			for(int j=0; j<100; j++) {
-				if(map[i][j] == road) {
-					tempY = i;
-					tempX = j;
-					
-					maxY = Math.max(maxY, tempY);
-					maxX = Math.max(maxX, tempX);
-					minY = Math.min(minY, tempY);
-					minX = Math.min(minX, tempX);
-				}
+
+	static void print() {
+		for (int i = start_y; i <= end_y; i++) {
+			if (i != start_y)
+				System.out.println();//출력 시 첫 줄에 빈 줄 나오는 거 방지
+			for (int j = start_x; j <= end_x; j++) {
+				if (map[i][j] != '.')
+					map[i][j] = '#';
+				System.out.print(map[i][j]);
 			}
-		}
-	
-		
-		for(int i=minY; i<=maxY; i++) {
-			for(int j=minX; j<=maxX; j++) {
-				if(map[i][j] != road) {
-					map[i][j] = wall;
-				}
-				
-				sb.append(map[i][j]);
-			}
-			sb.append("\n");
+
 		}
 
-	}
-	
+	}// 맵 범위 빈 칸에 #찍고 출력
 }
