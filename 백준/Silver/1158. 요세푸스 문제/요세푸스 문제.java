b538b1e86ef9a402070c32ebf0784e5ffbb03408
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-08 11:36:52 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 564 ms, Memory: 295256 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,41 +1,47 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int N, K;
-	static Deque<Integer> circle = new ArrayDeque<Integer>();// 입력 값 저장 덱
-	static Deque<Integer> answer = new ArrayDeque<Integer>();// 답 저장 덱
+	static int N,M;
+	static Queue<Integer> q = new ArrayDeque<>();  //요세푸스 순열 저장
+	static LinkedList<Integer> l = new LinkedList<>(); //사람들
 
-	public static void main(String[] args) throws IOException {
-		input();
-		kill();
-
-		System.out.println(answer.toString().replace('[', '<').replace(']', '>'));
-	}
-
-	static void input() throws IOException {
-		st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws Exception{ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
 		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		for (int i = 1; i < N + 1; i++) {
-			circle.add(i);
-
-		} // circle에 1~N까지의 수를 채워줌
-	}
-
-	static void kill() {
-		while (!circle.isEmpty()) {
-			for (int i = 0; i < K - 1; i++) {
-				circle.add(circle.poll());
-			}
-			answer.add(circle.poll());
+		M = Integer.parseInt(st.nextToken());
+	
+		for(int i=1; i<=N; i++) {
+			l.add(i);
 		}
-	}// 순환하면서 K번째 수마다 제거
+		
+		while(!l.isEmpty()) {
+			
+			for(int i=0; i<M-1; i++) {
+				int temp = 0;
+				temp = l.poll();
+				l.add(temp);
+			}
+			
+			q.add(l.poll());
+		}
+		
+		sb.append("<");
+		while(!q.isEmpty()) {
+			sb.append(q.poll()).append(", ");
+		}
+		sb.deleteCharAt(sb.length()-1);
+		sb.deleteCharAt(sb.length()-1);
+		sb.append(">");
+		
+		System.out.println(sb);
+	}
 
 }
