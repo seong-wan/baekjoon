6a6381484da6ed211270768f4c614c03917209d2
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-09 12:19:06 +0900

    [Silver I] Title: 절댓값 힙, Time: 788 ms, Memory: 29564 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-//PriorityQueue를 사용하지 않는 <= 배열을 이용해서 직접구현(힙 구현x)
+import java.util.PriorityQueue;
+//PriorityQueue를 사용하는
 //10만개의 정수 배열을 만들고, from, to index 를 이용
 public class Main {
 	//1.입력값이 0이면 배열에서 절댓값이 가장 작은 값 출력, 제외
@@ -9,7 +10,9 @@ public class Main {
 	//3.배열이 비어있는데 입력 값이 0 이면 0출력
 	
 	static int N, from, to;
-	static int[] array = new int[100000];
+	static PriorityQueue<Integer> pqueue = new PriorityQueue<>(
+	(n1, n2) -> Math.abs(n1) == Math.abs(n2) ? n1 - n2 : Math.abs(n1) - Math.abs(n2)	
+	);
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
@@ -17,43 +20,14 @@ public class Main {
 		for(int i=0; i<N; i++) {
 			int num = Integer.parseInt(br.readLine());
 			if(num == 0) {
-				check();
+				Integer min = pqueue.poll();
+				System.out.println(min == null ? 0 : min);
 			}else {
-				array[to++] = num;
+				pqueue.offer(num);
 			}
 		}
 		
 	}
 	
-	static void check() {
-		// 대상이 없을 때 0 <= 최초, 다 꺼냈을 때
-		if(from == to) {
-			System.out.println(0);
-			return;
-		}
-		
-		//최소 값 (최소 값의 index)
-		int minNum = Integer.MAX_VALUE;
-		int minIdx = from;
-		
-		for(int i=from; i<to; i++) {
-			//array[i] 를 따져본다.
-			if(Math.abs(minNum) == Math.abs(array[i])) {//현재 최소 절대값과 같은 경우
-				if(array[i] < minNum) {
-					minNum = array[i];
-					minIdx = i;
-				}
-			}else if(Math.abs(minNum) > Math.abs(array[i])) {//현재 최소 절대값보다 큰 경우
-				minNum = array[i];
-				minIdx = i;
-			}
-		}
-		
-		System.out.println(minNum); //출력
-		//최소값을 빼는 부분 <=from 과 바꿔치기
-		array[minIdx] = array[from];
-		from++;
-		
-	}
 
 }
