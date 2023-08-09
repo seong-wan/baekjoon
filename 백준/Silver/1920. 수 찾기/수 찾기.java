fbaa2acbdbc9fd0d220b85b287bb2101cc5f59cb
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-09 14:08:53 +0900

    [Silver IV] Title: 수 찾기, Time: 1252 ms, Memory: 55024 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,31 +1,37 @@
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+	//존재하면 1\
+	static int N,M;
+	static int[] arr;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i=0; i<M; i++) {
+			int a = Integer.parseInt(st.nextToken());
+			
+			if(Arrays.binarySearch(arr, a) < 0) {
+				System.out.println(0);
+			}else {
+				System.out.println(1);
+			}
+		}
+	}
 
-        HashSet<Integer> A = new HashSet<>(); //중복 x
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            A.add(Integer.parseInt(st.nextToken()));
-        } // 비교 대상 HashSet A
-
-        int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            int b = Integer.parseInt(st.nextToken());
-            if (A.contains(b)) {
-                sb.append(1).append(" ").append("\n");//b가 존재하면 1출력
-            } else {
-                sb.append(0).append(" ").append("\n");//b가 존재하지 않으면 0출력
-            }
-        }
-        System.out.println(sb);
-    }
 }
