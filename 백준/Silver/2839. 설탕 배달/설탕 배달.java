a1785b48d7e32058d0a0f4377d3697efe747931b
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-11 14:58:03 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,30 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int N;
+	static int[] dp;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int cnt = 0;
-		
-		while(true) {
-			
-			if(N%5 == 0) {
-				cnt += N/5;
-				break;
-			}
-			
-			N -= 3;
-			cnt++;
-			if(N == 0) break;
-			
-			if(N<0) {
-				cnt = -1;
-				break;
-			}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		N = Integer.parseInt(br.readLine());
+
+		if (N > 5) {
+			dp = new int[N + 1];
+
+			dp[1] = Integer.MAX_VALUE/2;
+			dp[2] = Integer.MAX_VALUE/2;
+			dp[3] = 1;
+			dp[4] = Integer.MAX_VALUE/2;
+			dp[5] = 1;
+
+			for (int i = 6; i < N + 1; i++) {
+				dp[i] = Math.min(dp[i - 3] + 1, dp[i - 5] + 1);
+
+			}if(dp[N]>=Integer.MAX_VALUE/2)
+                	System.out.println(-1);
+
+			else System.out.println(dp[N]);
+		} else {
+			if (N == 3||N==5)
+				System.out.println(1);
+			else
+				System.out.println(-1);
 		}
-		
-		System.out.println(cnt);
 
 	}
 
