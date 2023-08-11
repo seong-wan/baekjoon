76d7b01c1a2a8b0c59d549b833d3d18c99d43d24
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-11 17:46:43 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 128 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -3,45 +3,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static int[] dwarf = new int[9];//난쟁이 9명의 숫자가 들어갈 배열
-	static int sum;//9명의 숫자의 합
-	static int dif;//숫자의 합과 100의 차이
-	static int rem1, rem2;//없애야 할 난쟁이의 인덱스
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		input();
-		chk();
-		print();
-	}
-
-	static void input() throws NumberFormatException, IOException {
-		for (int i = 0; i < 9; i++) {
-			sum += dwarf[i] = Integer.parseInt(br.readLine());
+	static int[] src = new int[9];
+	static int[] tgt = new int[7];
+	static StringBuilder sb = new StringBuilder();
+	static int sum;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for(int i=0; i<9; i++) {
+			src[i] = Integer.parseInt(br.readLine());
 		}
-		dif = sum - 100;
-	}//난쟁이 숫자를 전부 배열에 넣으면서 합과 100의 차이를 구함
-
-	static void chk() {
-		for (int i = 0; i < 9; i++) {
-
-			for (int j = i + 1; j < 9; j++) {
-				if (dwarf[i] + dwarf[j] == dif) {
-					rem1 = i;
-					rem2 = j;
-					break;
+		
+		sum = 0;
+		comb(0,0);
+		
+		System.out.println(sb);
+	}	
+	
+	static void comb(int srcIdx, int tgtIdx) {
+		if(tgtIdx == 7) {
+			if(sum == 100) {
+				for (int i : tgt) {
+					sb.append(i).append("\n");
 				}
-
+				return;
+			}else {
+				return;
 			}
-		}//for문을 돌면서 숫자의 합이 dif와 같은 난쟁이 2명의 인덱스를 구함
-	}
-
-	static void print() {
-		for (int i = 0; i < 9; i++) {
-			if (i == rem1 || i == rem2)
-				continue;
-			System.out.println(dwarf[i]);
 		}
-	}//제거할 난쟁이를 제외한 난쟁이들의 숫자를 출력
-
+		
+		if(srcIdx == 9) return;
+		
+		tgt[tgtIdx] = src[srcIdx];
+		sum += tgt[tgtIdx];
+		comb(srcIdx + 1, tgtIdx + 1);
+		sum -= tgt[tgtIdx];
+		comb(srcIdx + 1, tgtIdx);
+	}
 }
