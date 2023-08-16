fcfa651d7557938a93cdff9163fcf696ac56bc57
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-16 09:52:42 +0900

    [Silver II] Title: A → B, Time: 128 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -4,30 +4,39 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int A, B, cnt;// 바꿀 수,목표 수,바꾸는 데 필요한 연산
-	static boolean chk;// 목표값으로 바뀌는지 체크
-
+	static int cnt;
 	public static void main(String[] args) throws IOException {
-		st = new StringTokenizer(br.readLine());
-		A = Integer.parseInt(st.nextToken());
-		B = Integer.parseInt(st.nextToken());
-		dfs(A, 1);// A를 cnt 1부터 시작하면서 연산을 해봄
-		if (!chk)
-			System.out.println(-1);// 변경 불가능하면 -1출력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		cnt = 1;
+		simulation(a,b);
+		System.out.println(cnt);
 	}
-
-	static void dfs(long num, int cnt) {
-		if (num > B)
-			return;
-		if (num == B) {
-			chk = true;
-			System.out.println(cnt);
-			return;
+	static void simulation(int a, int b) {
+		
+		while(b != a) {
+			if(b < a) {
+				cnt = -1;
+				return;
+			}
+			
+			if(b % 2 != 0 && (b-1)%10 != 0) {
+				cnt = -1;
+				return;
+			}
+			
+			if(b % 2 == 0) {
+				b /= 2;
+			}else {
+				b = (b-1)/10;
+			}
+			
+			cnt++;
 		}
-		dfs(num * 2, cnt + 1);
-		dfs(num * 10 + 1, cnt + 1);
+		
 	}
-
 }
