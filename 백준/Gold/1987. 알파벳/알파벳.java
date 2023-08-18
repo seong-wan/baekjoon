d21a582b3bc46f34939c6d3395c27eb4d32c4e29
seong-wan <cjddktkdwk@naver.com>
2023-08-18 17:47:29 +0900

    Update 알파벳.java

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -7,7 +7,7 @@ public class Main {
 	static StringTokenizer st;
 	static int R, C, ans;
 	static char[][] map;
-	static boolean[] visited = new boolean[26];
+	static boolean[] visited = new boolean[26];//알파벳별 방문 표시
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };// 상,하,좌,우
 
@@ -19,8 +19,8 @@ public class Main {
 		for (int i = 0; i < R; i++) {
 			map[i] = br.readLine().toCharArray();
 		} // 맵 입력
-		visited[map[0][0] - 65] = true;
-		dfs(0, 0, 1);
+		visited[map[0][0] - 65] = true;//말이 있는 처음 위치 알파벳 방문 표시
+		dfs(0, 0, 1);//처음 칸 포함이므로 cnt=1로 시작
 		System.out.println(ans);
 	}
 
@@ -31,9 +31,9 @@ public class Main {
 			int nr = r + dr[dir];
 			int nc = c + dc[dir];
 			if (nr >= 0 && nr <= R - 1 && nc >= 0 && nc <= C - 1 && !visited[map[nr][nc] - 65]) {
-				visited[map[nr][nc] - 65] = true;
+				visited[map[nr][nc] - 65] = true;//갔던 알파벳 방문 표시
 				dfs(nr, nc, cnt + 1);
-				visited[map[nr][nc] - 65] = false;
+				visited[map[nr][nc] - 65] = false;//갔던 길 되돌아갈 때 원복
 			}
 		}
 	}
