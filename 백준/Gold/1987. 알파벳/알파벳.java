ddca7df2e3ef84aa9ba6f13854c16932dff07282
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-18 17:37:20 +0900

    [Gold IV] Title: 알파벳, Time: 900 ms, Memory: 15208 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,49 +1,41 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] arr;
-    static int r, c;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int max = 0; // 최대 방문 횟수
-    static boolean[] alpha; // 방문한 알파벳인지 확인
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int R, C, ans;
+	static char[][] map;
+	static boolean[] visited = new boolean[26];
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };// 상,하,좌,우
 
-    public static void main(String[] args)  {
-        Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+		st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		map = new char[R][C];
+		for (int i = 0; i < R; i++) {
+			map[i] = br.readLine().toCharArray();
+		} // 맵 입력
+		visited[map[0][0] - 65] = true;
+		dfs(0, 0, 1);
+		System.out.println(ans);
+	}
 
-        r = scan.nextInt();
-        c = scan.nextInt();
-        scan.nextLine();
+	static void dfs(int r, int c, int cnt) {
 
-        arr = new int[r][c];
+		ans = Math.max(ans, cnt);
+		for (int dir = 0; dir < 4; dir++) {
+			int nr = r + dr[dir];
+			int nc = c + dc[dir];
+			if (nr >= 0 && nr <= R - 1 && nc >= 0 && nc <= C - 1 && !visited[map[nr][nc] - 65]) {
+				visited[map[nr][nc] - 65] = true;
+				dfs(nr, nc, cnt + 1);
+				visited[map[nr][nc] - 65] = false;
+			}
+		}
+	}
 
-        for (int i = 0; i < r; i++) {
-            String str = scan.nextLine();
-            for (int j = 0; j < c; j++) {
-                arr[i][j] = str.charAt(j) - 'A';
-            }
-        }
-
-        alpha = new boolean[26];
-        dfs(0, 0, 1);
-        System.out.println(max);
-    }
-
-    //len은 몇 번 지났는지
-    public static void dfs(int x, int y, int len){
-        alpha[arr[x][y]] = true; // 알파벳 방문 체크
-        max = Math.max(max, len);
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx >= 0 && ny >=0 && nx < r && ny < c){
-                if(alpha[arr[nx][ny]] == false){
-                    dfs(nx, ny, len+1);
-                    //갈 수 있는 길 하나를 끝까지 가봤으면 다시 알파벳 방문을 초기화해줘야함
-                    alpha[arr[nx][ny]] = false;
-                }
-            }
-        }
-    }
 }
