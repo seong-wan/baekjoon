02b5e6999a03be21ebee4092a4d81e9c774e65d5
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-21 16:45:43 +0900

    [Silver I] Title: 숨바꼭질, Time: 156 ms, Memory: 16300 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -3,51 +3,59 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
-
+//그래프 문제로 생각해보기
+//bfs -> 최단경로
+//visit
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int N, K;// 수빈이 위치, 동생 위치
-
-	static int[] visited = new int[100_001];
-
-	public static void main(String[] args) throws Exception {
-		st = new StringTokenizer(br.readLine());
+	
+	static int N,K;
+	static Queue<Integer> queue = new ArrayDeque<>();
+	static int[] visit = new int[100001]; //boolean 이 아닌 int 쓰는 이유? -> visit자체에 몇번재 depth인지 기록
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());// 수빈이와 동생 위치 입력
+		K = Integer.parseInt(st.nextToken());
 
-		bfs(N);
-		System.out.println(visited[K]);
+		System.out.println(bfs(N, K));
 	}
-
-	static void bfs(int N) {
-		Queue<Integer> queue = new ArrayDeque<Integer>();
-		queue.add(N);
-		visited[N] = 0;// 시작점을 1로 시작 결과에서 -1해줘야 함
-		while (!queue.isEmpty()) {
-			int x = queue.poll();
-			if (x == K) {
-				return;
-			} // K에 도달하면 bfs종료
-			int nx = x + 1;
-
-			if (nx >= 0 && nx <= 100_000 && visited[nx] == 0) {
-				visited[nx] = visited[x] + 1;
-				queue.add(nx);
+	
+	static int bfs(int N, int K) {
+		// 최소 초를 계산해서 return하기
+		int minus = 0; //뺀값
+		int plus = 0; //더한값
+		int multiply = 0; //2배한 값
+		
+		visit[N] = 0; //depth계산을 위한 0
+		queue.offer(N);
+		
+		while(!queue.isEmpty()) {
+			// 꺼내서 K이면 return;
+			int curr = queue.poll();
+			if(curr == K) break;
+			
+			//그렇지 않으면 꺼내서 +1, -1, *2 에 대한 계산을 하고 유효하면 queue에 넣기
+			minus = curr - 1;
+			plus = curr + 1;
+			multiply = curr * 2;
+			
+			if(minus >= 0 && visit[minus] == 0) {//음수가 아니고 방문하지 않았다면
+				visit[minus] = visit[curr] + 1; //꺼낸 curr까지 온 depth보다 한 번 더 계산
+				queue.offer(minus);
 			}
-			nx = x - 1;
-			if (nx >= 0 && nx <= 100_000 && visited[nx] == 0) {
-				visited[nx] = visited[x] + 1;
-				queue.add(nx);
+			
+			if(plus <= 100000 && visit[plus] == 0) {
+				visit[plus] = visit[curr] + 1; 
+				queue.offer(plus);
 			}
-			nx = x * 2;
-			if (nx >= 0 && nx <= 100_000 && visited[nx] == 0) {
-				visited[nx] = visited[x] + 1;
-				queue.add(nx);
+			
+			if(multiply <= 100000 && visit[multiply] == 0) {
+				visit[multiply] = visit[curr] + 1; 
+				queue.offer(multiply);
 			}
-
 		}
-
+		return visit[K]; //boolean의 방문 여부 체크  + //K까지 오는데 몇번 걸렸는지 저장되잇음
 	}
-
 }
