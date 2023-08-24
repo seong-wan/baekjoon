df5054608d4af3de8da1381777be6de5626f0555
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-24 12:16:31 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 505 ms, Memory: 94,696 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,19 +1,18 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
 	static class Edge {
-		int from, to;
+		int v;
 		double weight;
 
-		public Edge(int from, int to, double weight) {
+		public Edge(int v, double weight) {
 			super();
-			this.from = from;
-			this.to = to;
+			this.v = v;
 			this.weight = weight;
 		}
 
@@ -23,41 +22,21 @@ public class Solution {
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
 	static int T, N;
-	static double E;
+	static double E, result;
 	static int[] X, Y;
-	static int parents[];
-	static List<Edge> edgeList;
-	static int V, Edge;
-
-	static void make() {
-		parents = new int[N];
-		for (int i = 0; i < N; i++) {
-			parents[i] = i;// 모든 요소는 자기만 원소로 갖는 단위 서로소 집합이 되게 한다.(자신이 곧 대표자인 루트로 표현)
-		}
-	}
-
-	static int find(int a) {
-		if (parents[a] == a)
-			return a;
-		return parents[a] = find(parents[a]);
-	}
-
-	static boolean union(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-		if (aRoot == bRoot)
-			return false;
-		parents[bRoot] = aRoot;
-		return true;
-
-	}
+	static List<List<Edge>> adlist;
+	static boolean[] visit;
+	static PriorityQueue<Edge> pq = new PriorityQueue<>(((e1, e2) -> e1.weight - e2.weight >= 0 ? 1 : -1));
 
 	public static void main(String[] args) throws Exception {
 		T = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= T; t++) {
+			result = 0;
+			pq.clear();
 			N = Integer.parseInt(br.readLine());
 			X = new int[N];
 			Y = new int[N];
+			visit = new boolean[N];
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
 				X[i] = Integer.parseInt(st.nextToken());
@@ -69,19 +48,10 @@ public class Solution {
 
 			} // Y좌표 입력
 			E = Double.parseDouble(br.readLine());
+
 			make_edge();
-			make();
 
-			double result = 0;// MST 비용
-			int count = 0;// 연결된 간선 개수
-			for (Edge e : edgeList) {
-				if (union(e.from, e.to)) {
-					result += e.weight;
-					if (++count == N - 1)
-						break;
-				}
-
-			}
+			prim();
 			sb.append("#" + t + " " + Math.round(result) + "\n");
 
 		}
@@ -90,17 +60,38 @@ public class Solution {
 	}
 
 	static void make_edge() {
-		edgeList = new ArrayList<>();
+		adlist = new ArrayList<>();
+		for (int i = 0; i <= N; i++) {
+			adlist.add(new ArrayList<>());
+
+		}
+
 		for (int i = 0; i < N - 1; i++) {
 			for (int j = i + 1; j < N; j++) {
 				int from = i;
 				int to = j;
 				double w = E * (Math.pow(X[i] - X[j], 2) + Math.pow(Y[i] - Y[j], 2));
-				edgeList.add(new Edge(from, to, w));
+				adlist.get(from).add(new Edge(to, w));
+				adlist.get(to).add(new Edge(from, w));
 			}
 		}
-		Collections.sort(edgeList, (e1, e2) -> e1.weight - e2.weight >= 0 ? 1 : -1);
-		// 가중치 기준 오름차순으로 정렬
 	}// 각 섬 간의 환경 부담금을 계산하여 간선 리스트에 넣음
+
+	static void prim() {
+		int cnt = 1; // 선택된 정점의 수
+		visit[0] = true;// 시작 정점 visit(선택)
+		pq.addAll(adlist.get(0));
+		while (!pq.isEmpty()) {
+			Edge edge = pq.poll();// 현재 pq에 담긴 간선들 중 비용이 최소
+			if (!visit[edge.v]) {
+				visit[edge.v] = true;
+				result += edge.weight;
+				cnt++;
+				if (cnt == N)
+					break;
+				pq.addAll(adlist.get(edge.v));
+			}
+		}
+	}
 
 }
