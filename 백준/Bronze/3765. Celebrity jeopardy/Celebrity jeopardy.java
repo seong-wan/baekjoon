2fe4478970ea7322f3553bea3828ca487a600432
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-27 01:41:40 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 120 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
+		String s;
+		while ((s = br.readLine()) != null) {
+			System.out.println(s);
+
+		}
+
+	}
+
+}
