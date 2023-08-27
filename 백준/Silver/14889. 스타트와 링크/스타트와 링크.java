cc1c8f55d031f23128fafae65205513a3710482e
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-27 13:10:23 +0900

    [Silver II] Title: 스타트와 링크, Time: 328 ms, Memory: 41564 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -7,13 +7,13 @@ public class Main {
 	static StringTokenizer st;
 	static int N, ans = Integer.MAX_VALUE;// 사람의 수 , 능력치의 합,최소값
 	static int[][] ability, synergy;// 능력치를 입력 받을 배열, 시너지를 따로 계산해놓을 배열
-	static int[] tgt;// 조합
+	static boolean[] visit;// 0은 더미
 
 	public static void main(String[] args) throws Exception {
 		N = Integer.parseInt(br.readLine());
 		ability = new int[N + 1][N + 1];
 		synergy = new int[N + 1][N + 1];// 0은 더미
-		tgt = new int[N / 2];
+		visit = new boolean[N + 1];
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= N; j++) {
@@ -22,7 +22,7 @@ public class Main {
 			}
 		} // 능력치의 합을 구하면서 입력 받음
 		syn();// 두 사람 사이의 시너지를 미리 계산해서 배열에 넣음
-		comb(0, 1);//1의 번호부터 N번호 중 N/2 명의 사람의 조합을 구함
+		comb(0, 1);// 1의 번호부터 N번호 중 N/2 명의 사람의 조합을 구함
 		System.out.println(ans);
 	}
 
@@ -33,28 +33,42 @@ public class Main {
 		}
 		if (num == N + 1)
 			return;
-		tgt[tgtidx] = num;
+		visit[num] = true;
 		comb(tgtidx + 1, num + 1);
+		visit[num] = false;
 		comb(tgtidx, num + 1);
-	}//한 쪽 팀의 조합을 구함
+	}// 한 쪽 팀의 조합을 구함
 
 	static void cal() {
-		int tgtsum = 0;
-		int remainsum = 0;
-		boolean[] visit = new boolean[N + 1];// 0은 더미
-		for (Integer num : tgt) {
-			visit[num] = true;//뽑은 사람들의 번호는 true로 나머지는 false로 해서 팀을 나눔
-		}
-		for (int i = 1; i <= N - 1; i++) {
-			for (int j = i + 1; j <= N; j++) {
-				if (visit[i] == true && visit[j] == true)
-					tgtsum += synergy[i][j];//뽑힌 사람들의 시너지의 합
-				if (visit[i] == false && visit[j] == false)
-					remainsum += synergy[i][j];//뽑히지 않은 사람들의 시너지의 합
-			}
+		int[] Ateam = new int[N / 2];
+		int[] Bteam = new int[N / 2];
+		int Asum = 0;
+		int Bsum = 0;
+		int Aidx = 0;
+		int Bidx = 0;
+		for (int i = 1; i <= N; i++) {
+			if (visit[i])
+				Ateam[Aidx++] = i;
+			else
+				Bteam[Bidx++] = i;
 		}
 
-		ans = Math.min(ans, Math.abs(tgtsum - remainsum));// 양팀 시너지 차이의 최소값을 구함
+//		for (int i = 1; i <= N - 1; i++) {
+//			for (int j = i + 1; j <= N; j++) {
+//				if (visit[i] == true && visit[j] == true)
+//					tgtsum += synergy[i][j];// 뽑힌 사람들의 시너지의 합
+//				if (visit[i] == false && visit[j] == false)
+//					remainsum += synergy[i][j];// 뽑히지 않은 사람들의 시너지의 합
+//			}
+//		}
+		for (int i = 0; i < N / 2 - 1; i++) {
+			for (int j = i + 1; j < N / 2; j++) {
+				Asum += synergy[Ateam[i]][Ateam[j]];
+				Bsum += synergy[Bteam[i]][Bteam[j]];
+			}
+		} // visit를 계속 확인하면서 구하지 않고 팀별로 배열에 미리 넣고 구하면 시간이 덜 걸림
+
+		ans = Math.min(ans, Math.abs(Asum - Bsum));// 양팀 시너지 차이의 최소값을 구함
 
 	}
 
