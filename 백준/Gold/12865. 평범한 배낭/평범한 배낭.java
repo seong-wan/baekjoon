77c6a47083fbc2ecbd11d54a93c7fb84b9d8ef6a
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-29 17:31:50 +0900

    [Gold V] Title: 평범한 배낭, Time: 164 ms, Memory: 15492 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -3,39 +3,28 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N,K;
-	static int[][] item;
-	static int[] memoi;
-	
-	public static void main(String[] args) throws Exception{
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		
-		item = new int[N+1][2];
-		memoi = new int[K+1];
-		
-		for(int i=1; i<=N; i++) {
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int memo[] = new int[K + 1];// 0은 더미
+
+		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<2; j++) {
-				item[i][j] = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+
+			for (int w1 = K; w1 >= w; w1--) {
+
+				memo[w1] = Math.max(memo[w1], memo[w1 - w] + v);
+
 			}
 		}
-		
-		//memoi[] 배열을 1차원으로 선언하는 방식
-		
-		for(int i=1; i<=N; i++) {
-			//가장 큰 무게부터 작은 무게로 움직이면서
-			for(int j = K ; j >= item[i][0]; j--) {
-				//memoi[j] => i번째 item을 고려하기 전까지 w무게를 만드는데 최상의 value
-				//memoi[j] 를 새로운 현재 고려하는 값으로 덮어쓰지 않으면 이전 item을 고려한 최상의 value
-				memoi[j] = Math.max(memoi[j], memoi[j-item[i][0]] + item[i][1]);
-			}
-		}
-		
-		System.out.println(memoi[K]);
+
+		System.out.println(memo[K]);
+
 	}
 
 }
