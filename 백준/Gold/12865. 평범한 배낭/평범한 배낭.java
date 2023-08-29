568db5b2baf5260e6fc6dd6e7fd65714397c2317
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-29 17:25:29 +0900

    [Gold V] Title: 평범한 배낭, Time: 164 ms, Memory: 15496 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -7,24 +7,21 @@ public class Main {
 	static StringTokenizer st;
 	static int N, K;// 물품의 수, 제한 무게
 	static int memo[];// [N][K]가 구하는 가치합의 최댓값
-	static int item[][];
 
 	public static void main(String[] args) throws Exception {
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		item = new int[N + 1][2];
 		memo = new int[K + 1];// 0은 더미
 
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int w = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
-			item[i][0] = w;
-			item[i][1] = v;
-			for (int w1 = K; w1 >= item[i][0]; w1--) {
 
-				memo[w1] = Math.max(memo[w1], memo[w1 - item[i][0]] + item[i][1]);
+			for (int w1 = K; w1 >= w; w1--) {
+
+				memo[w1] = Math.max(memo[w1], memo[w1 - w] + v);
 
 			}
 		}
