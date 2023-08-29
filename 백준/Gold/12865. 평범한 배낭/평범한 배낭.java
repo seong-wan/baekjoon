2e3fa58f4abf6ee737fbf4c3e2bf144167962460
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-29 17:22:41 +0900

    [Gold V] Title: 평범한 배낭, Time: 180 ms, Memory: 16280 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -6,7 +6,7 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static int N, K;// 물품의 수, 제한 무게
-	static int memo[][];// [N][K]가 구하는 가치합의 최댓값
+	static int memo[];// [N][K]가 구하는 가치합의 최댓값
 	static int item[][];
 
 	public static void main(String[] args) throws Exception {
@@ -14,7 +14,7 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		item = new int[N + 1][2];
-		memo = new int[N + 1][K + 1];// 0은 더미
+		memo = new int[K + 1];// 0은 더미
 
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -22,24 +22,14 @@ public class Main {
 			int v = Integer.parseInt(st.nextToken());
 			item[i][0] = w;
 			item[i][1] = v;
-		}
+			for (int w1 = K; w1 >= item[i][0]; w1--) {
 
-		for (int i = 1; i <= N; i++) {
-			for (int w = 1; w <= K; w++) {
-				if (item[i][0] <= w) {
-					// 선택
-					int sel = memo[i][w] = memo[i - 1][w - item[i][0]] + item[i][1];
-					int nosel = memo[i - 1][w];
-					memo[i][w] = Math.max(sel, nosel);
-
-				} else {
-					memo[i][w] = memo[i - 1][w];
-				}
+				memo[w1] = Math.max(memo[w1], memo[w1 - item[i][0]] + item[i][1]);
 
 			}
-
 		}
-		System.out.println(memo[N][K]);
+
+		System.out.println(memo[K]);
 
 	}
 
