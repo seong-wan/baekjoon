625bf9b58cf3ce85fb27035dcc79f3793f002781
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-30 11:43:28 +0900

    [Silver V] Title: 다리 놓기, Time: 152 ms, Memory: 14684 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,41 +1,33 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int T, N, M;// 테스트 케이스,서쪽 사이트, 동쪽 사이트
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int T;
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
-	static int[][] arr;
-	static int ans;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws Exception {
 		T = Integer.parseInt(br.readLine());
+		int[][] memoi = new int[31][31];
+
+		for (int i = 1; i <= 30; i++) {
+			for (int j = 0; j <= 30; j++) {
+				if (j == 0 || i == j)
+					memoi[i][j] = 1;
+				else
+					memoi[i][j] = (memoi[i - 1][j - 1] + memoi[i - 1][j]);
+			}
+		}
 		for (int t = 0; t < T; t++) {
-			input();
-			sb.append(comb(M, N) + "\n");
+			st = new StringTokenizer(br.readLine());
+			int M = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
+			sb.append(memoi[N][M] + "\n");
 
 		}
 		System.out.println(sb);
 	}
 
-	static void input() throws NumberFormatException, IOException {
-
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[M + 1][N + 1];
-	}
-
-	static int comb(int M, int N) {
-		if (N == 0 || M == N) {
-			return arr[M][N] = 1;
-		} else {
-			if (arr[M][N] != 0)
-				return arr[M][N];//구했던 부분은 다시 구하지 않고 메모이제이션을 이용함
-			return arr[M][N] = comb(M - 1, N - 1) + comb(M - 1, N);
-		}
-	}// 조합식을 나눠서 조합의 개수를 구함
 }
