f6fcc80a3c2f5f4cceb87f8b35ed8055e940e3de
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-08-30 12:25:17 +0900

    [Silver V] Title: 다리 놓기, Time: 172 ms, Memory: 16288 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,33 +1,40 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-
+//comb는 시간초과
+//dp + 파스칼의 방정식 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static int T;
-	static StringTokenizer st;
+	static int T,N,M;
+	static int[][] memoi;
 	static StringBuilder sb = new StringBuilder();
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		T = Integer.parseInt(br.readLine());
-		int[][] memoi = new int[31][31];
-
-		for (int i = 1; i <= 30; i++) {
-			for (int j = 0; j <= 30; j++) {
-				if (j == 0 || i == j)
-					memoi[i][j] = 1;
-				else
-					memoi[i][j] = (memoi[i - 1][j - 1] + memoi[i - 1][j]);
+		
+		for(int t=1; t<=T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			memoi = new int[M+1][M+1]; //0dummy, M*N (x)
+			
+			memoi[0][0] = 1;
+			
+			//파스칼의 삼각형
+			for(int i=1; i<=M; i++) { //밑으로 (행)
+				for(int j=0; j<=i; j++) { //옆으로 (열)
+					if(j==0 || i==j) {
+						memoi[i][j] = 1;
+						continue;
+					}
+					
+					memoi[i][j] = memoi[i-1][j-1] + memoi[i-1][j];
+				}
 			}
+			
+			System.out.println(memoi[M][N]);
 		}
-		for (int t = 0; t < T; t++) {
-			st = new StringTokenizer(br.readLine());
-			int M = Integer.parseInt(st.nextToken());
-			int N = Integer.parseInt(st.nextToken());
-			sb.append(memoi[N][M] + "\n");
-
-		}
-		System.out.println(sb);
 	}
-
+	
+	
 }
