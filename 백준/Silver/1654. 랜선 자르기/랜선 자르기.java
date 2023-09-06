554e9383081f15a6a4c94ff867ce16fcf59411a6
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-09-06 11:24:49 +0900

    [Silver II] Title: 랜선 자르기, Time: 184 ms, Memory: 17192 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,44 +1,44 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int K, N;// 가지고 있는 랜선, 필요한 랜선
-	static int[] lan;
 
-	public static void main(String[] args) throws Exception {
-		st = new StringTokenizer(br.readLine());
+	static int N,K;
+	static int[] lanes;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		K = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
-		lan = new int[K];
+		lanes = new int[K];
 		for (int i = 0; i < K; i++) {
-			lan[i] = Integer.parseInt(br.readLine());
-
-		} // 랜선 정보 저장
-		Arrays.sort(lan);
-		find();
-	}
-
-	static void find() {
-		long left = 1;
-		long right = lan[K - 1];
-		while (left <= right) {
-			long half = (left + right) / 2;
-			int count = 0;
-			for (int i = 0; i < K; i++) {
-				count += lan[i] / half;
-			}
-			if (count < N) {
-				right = half - 1;
-			} else {
-				left = half + 1;
-			}
-
+			lanes[i] = Integer.parseInt(br.readLine());
 		}
-		System.out.println(right);
+		// max값 찾기
+		long end=0;
+		long start=1;
+		long mid=0;
+		for (int i = 0; i < K; i++) {
+			end = Math.max(end, lanes[i]);
+		}
+		
+		while(start <= end) {
+			mid = (start + end)/2; // 나눌 전선길이
+			
+			int cnt=0;
+			for (int i = 0; i < K; i++) {
+				cnt+=lanes[i] / mid;
+			}
+			
+			if( cnt < N) { // 막대 길이가 큰 경우 이므로 end를 줄이자.
+				end = mid-1;
+			}else { // 막대 길이가 작거나 알맞은 경우이므로 start를 올리자. 
+				start = mid+1;
+			}
+			
+		}
+		System.out.println(end);
 	}
 
 }
