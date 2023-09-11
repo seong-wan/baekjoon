60d4a3aa00f45a901633b4ccaa76a145e0c1041a
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-09-11 17:42:00 +0900

    [Silver IV] Title: 보물, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -1,29 +1,31 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	// B의 높은 수에 A의 낮은 수를 차례대로 곱해주고 더하면 됨
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
-	static int N;
-	static PriorityQueue<Integer> pqA = new PriorityQueue<>(), pqB = new PriorityQueue<>((n1, n2) -> n2 - n1);
-    
-	// 정렬을 바로 하기 위해 PQ사용 A는 오름차,B는 내림차
+	static int N, A[], B[];
+	
 	public static void main(String[] args) throws Exception {
 		N = Integer.parseInt(br.readLine());
+		A = new int[N];
+		B = new int[N];
 
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			pqA.add(Integer.parseInt(st.nextToken()));
+			A[i] = Integer.parseInt(st.nextToken());
 		}
 
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			pqB.add(Integer.parseInt(st.nextToken()));
+			B[i] = Integer.parseInt(st.nextToken());
 		}
 
+		Arrays.sort(A);
+		Arrays.sort(B);
 		System.out.println(cal());
 
 	}
@@ -31,9 +33,9 @@ public class Main {
 	static int cal() {
 		int ans = 0;
 		for (int i = 0; i < N; i++) {
-			ans += pqA.poll() * pqB.poll();
+			ans += A[i] * B[N - 1 - i];
 		}
 		return ans;
-	}//계산 
+	}// 계산
 
 }
