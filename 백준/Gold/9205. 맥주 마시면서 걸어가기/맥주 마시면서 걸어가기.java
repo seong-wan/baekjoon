5cf5aab9c192b15c9e59804a5b5741a4e187c149
seong-wan <cjddktkdwk@naver.com>
2023-09-20 10:27:10 +0900

    Update 맥주 마시면서 걸어가기.java

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -4,7 +4,7 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static int t, n, to, des[][], start[] = new int[2];// 테케,편의점 수 ,현재 갈 곳,편의점과 페스티벌 좌표,집 위치[0]=x,[1]=y
@@ -26,7 +26,7 @@ public class Main {
 				des[j][0] = Integer.parseInt(st.nextToken());
 				des[j][1] = Integer.parseInt(st.nextToken());
 
-			}
+			}//편의점과 페스티벌의 위치
 			bfs();
 		}
 
@@ -36,7 +36,7 @@ public class Main {
 		Queue<int[]> queue = new ArrayDeque<>();
 		queue.add(start);
 		while (!queue.isEmpty()) {
-			if (visit[n]) {
+			if (visit[n]) {//페스티벌에 도착한다면
 				System.out.println("happy");
 				return;
 			}
@@ -44,14 +44,14 @@ public class Main {
 			for (int i = 0; i < n + 1; i++) {
 				if (!visit[i]) {
 					int dis = Math.abs(des[i][0] - cur[0]) + Math.abs(des[i][1] - cur[1]);
-					if (dis <= 1000) {
+					if (dis <= 1000) {//맥주 1박스로 갈 수 있는 최대 거리
 						visit[i] = true;
 						queue.add(new int[] { des[i][0], des[i][1] });
 					}
 				}
 			}
 		}
-		System.out.println("sad");
+		System.out.println("sad");//bfs를 돌았지만 페스티벌에 도착하지 못한 경우
 	}
 
 }
