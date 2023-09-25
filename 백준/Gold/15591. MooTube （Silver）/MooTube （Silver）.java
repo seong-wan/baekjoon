1a141ca112f4d898fa4463e63d20e7156f729b4f
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-09-25 17:47:26 +0900

    [Gold V] Title: MooTube (Silver), Time: 1264 ms, Memory: 79116 KB -BaekjoonHub

--- 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.java
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.java
@@ -1,60 +1,66 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static StringBuilder sb = new StringBuilder();
-	static int N, Q, cnt, k, v;// 정점의 수, 질문의 수,볼 수 있는 동영상의 수,USADO 기준,기준 정점
-	static List<int[]>[] adlist;
-	static boolean[] visit;
 
-	public static void main(String[] args) throws Exception {
-		st = new StringTokenizer(br.readLine());
+	static int N,Q;
+	static ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
+	static boolean[] visit;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		Q = Integer.parseInt(st.nextToken());
-
-		adlist = new List[N + 1];// 0은 더미
-
-		for (int i = 1; i <= N; i++) {
-			adlist[i] = new ArrayList<int[]>();
-		} // 인접리스트 초기화
-
-		for (int i = 0; i < N - 1; i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			adlist[from].add(new int[] { to, w });
-			adlist[to].add(new int[] { from, w });
+		for (int i = 0; i < N+1; i++) {
+			graph.add(new ArrayList<int[]>());
 		}
-
+		
+		for(int i=0; i<N-1; i++) {
+			st = new StringTokenizer(br.readLine());
+			int p = Integer.parseInt(st.nextToken());
+			int q = Integer.parseInt(st.nextToken());
+			int r = Integer.parseInt(st.nextToken());
+			graph.get(p).add(new int[] {q,r});
+			graph.get(q).add(new int[] {p,r});
+		}
 		for (int i = 0; i < Q; i++) {
-			visit = new boolean[N + 1];
-			cnt = 0;
+			visit = new boolean[N+1]; // 0 dummy
 			st = new StringTokenizer(br.readLine());
-			k = Integer.parseInt(st.nextToken());
-			v = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int cnt = 0;
+			
 			visit[v] = true;
-			dfs(v);
-			sb.append(cnt + "\n");
-		}
-		System.out.println(sb);
-	}
-
-	static void dfs(int n) {
-		for (int[] to : adlist[n]) {
-			if (!visit[to[0]] && to[1] >= k) {
-				visit[to[0]] = true;
-				cnt++;
-				dfs(to[0]);
-
+			Queue<int[]> queue = new ArrayDeque<>();
+			
+			for (int[] now : graph.get(v)) {
+				if( !visit[now[0]] && now[1] >= k) {
+					queue.offer(now);
+					visit[now[0]] = true;
+					cnt++;
+				}
 			}
+
+			while(! queue.isEmpty()) {
+				int[] now = queue.poll();
+				
+				for (int[] temp : graph.get(now[0])) {
+					if( !visit[temp[0]] && temp[1] >= k) {
+						cnt++;
+						queue.offer(temp);
+						visit[temp[0]] = true;
+						
+					}
+				}
+				
+			}
+			
+			System.out.println(cnt);
+			
 		}
-
 	}
-
 }
