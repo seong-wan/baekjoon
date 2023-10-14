e5773ce7fb042b5dc4dbac62b6a3e6d3629b4ce9
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-10-15 02:04:52 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,18 +1,14 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int hr=0;
-        int vr=0;
-        int total= brown+yellow;
         for(int i=1;i<=yellow;i++){
             if(yellow%i==0){
-                hr=yellow/i;
-                vr=i;
-                if((hr+2)*(vr+2)==total){
-                    int[] answer = {hr+2,vr+2};
+               int hr=yellow/i+2;
+               int vr=i+2;
+                if((hr)*(vr)==brown+yellow){
+                    int[] answer = {hr,vr};
                      return answer;
                 }
             }
         }
-        return null;
-    }
+        return null;  }
 }
