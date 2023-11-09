53bad68ee16b756ebaacf2d869f3c912ade70f02
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-11-09 10:07:01 +0900

    [Silver II] Title: 생태학, Time: 1084 ms, Memory: 88596 KB -BaekjoonHub

--- 백준/Silver/4358. 생태학/생태학.java
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -5,6 +5,7 @@ import java.util.TreeMap;
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static TreeMap<String, Float> map = new TreeMap<>();
+	static StringBuilder sb = new StringBuilder();
 	static float sum;
 
 	public static void main(String[] args) throws Exception {
@@ -20,6 +21,7 @@ public class Main {
 				map.put(s, (float) 1);
 		}
 		map.forEach((k, v) -> map.put(k, map.get(k) / sum * 100));
-		map.forEach((k, v) -> System.out.println(k + String.format(" %.4f", v)));
+		map.forEach((k, v) -> sb.append((k + String.format(" %.4f\n", v))));
+		System.out.println(sb);
 	}
 }
