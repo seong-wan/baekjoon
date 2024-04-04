f35b449663ad3299c79520c7965f3016bbd4189e
seong-wan <139252411+seong-wan@users.noreply.github.com>
2024-04-04 23:34:32 +0900

    [Bronze III] Title: 우유 축제, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/14720. 우유 축제/우유 축제.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int now = 0;
+        int ans = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (num == now) {
+                ans++;
+                now = (now + 1) % 3;
+            }
+        }
+        System.out.println(ans);
+    }
+}
