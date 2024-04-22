626789ef7fcbf5c668d5fd426c38da123c499d10
seong-wan <139252411+seong-wan@users.noreply.github.com>
2024-04-23 05:51:03 +0900

    [Bronze V] Title: Rook, Time: 112 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main(String[] args) {
+		String s = "  ___  ___  ___\n"
+			+ "  | |__| |__| |\n"
+			+ "  |           |\n"
+			+ "   \\_________/\n"
+			+ "    \\_______/\n"
+			+ "     |     |\n"
+			+ "     |     |\n"
+			+ "     |     |\n"
+			+ "     |     |\n"
+			+ "     |_____|\n"
+			+ "  __/       \\__\n"
+			+ " /             \\\n"
+			+ "/_______________\\";
+		System.out.print(s);
+	}
+}
