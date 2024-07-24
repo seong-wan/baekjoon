505dc1e71a886e149542ef29febda31d6671b9d6
seong-wan <139252411+seong-wan@users.noreply.github.com>
2024-07-25 04:21:20 +0800

    [Gold I] Title: 열쇠, Time: 260 ms, Memory: 24220 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.java
+++ 백준/Gold/9328. 열쇠/열쇠.java
@@ -1,10 +1,9 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
     static int T, h, w, ans;
     static char[][] map;
@@ -15,21 +14,21 @@ public class Main {
     static int[] dc = {0, -1, 0, 1};
 
     public static void main(String[] args) throws Exception {
-        T = Integer.parseInt(br.readLine());
+        Reader in = new Reader();
+
+        T = in.nextInt();
         for (int t = 0; t < T; t++) {
             start = new ArrayList<>();
             keys = new boolean[26];
             ans = 0;
 
-            st = new StringTokenizer(br.readLine());
-            h = Integer.parseInt(st.nextToken());
-            w = Integer.parseInt(st.nextToken());
+            h = in.nextInt();
+            w = in.nextInt();
 
             map = new char[h][w];
             for (int i = 0; i < h; i++) {
-                String s = br.readLine();
                 for (int j = 0; j < w; j++) {
-                    map[i][j] = s.charAt(j);
+                    map[i][j] = in.nextChar();
 
                     //가장자리에 있는 경우
                     if (i == 0 || i == h - 1 || j == 0 || j == w - 1) {
@@ -51,7 +50,7 @@ public class Main {
                 }
             }
 
-            String key = br.readLine();
+            String key = in.nextString();
             if (!key.equals("0")) {
                 int keySize = key.length();
                 for (int i = 0; i < keySize; i++) {
@@ -138,5 +137,97 @@ public class Main {
 
     static boolean canGo(int r, int c) {
         return r >= 0 && r <= h - 1 && c >= 0 && c <= w - 1;
+    }
+
+    static class Reader {
+        final int SIZE = 1 << 13;
+        byte[] buffer = new byte[SIZE];
+        int index, size;
+
+        String nextString() throws Exception {
+            StringBuilder sb = new StringBuilder();
+            byte c;
+            while ((c = read()) < 32) {
+                if (size < 0) return "endLine";
+            }
+            do sb.appendCodePoint(c);
+            while ((c = read()) > 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+            return sb.toString();
+        }
+
+        char nextChar() throws Exception {
+            byte c;
+            while ((c = read()) < 32) ; // SPACE 분리라면 <=로, SPACE 무시라면 <로
+            return (char) c;
+        }
+
+        int nextInt() throws Exception {
+            int n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) {
+                if (size < 0) return -1;
+            }
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        long nextLong() throws Exception {
+            long n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) ;
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        double nextDouble() throws Exception {
+            double n = 0, div = 1;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) {
+                if (size < 0) return -12345;
+            }
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            } else if (c == 46) {
+                c = read();
+            }
+            do n = (n * 10) + (c & 15);
+            while (isNumber(c = read()));
+            if (c == 46) {
+                while (isNumber(c = read())) {
+                    n += (c - 48) / (div *= 10);
+                }
+            }
+            return isMinus ? -n : n;
+        }
+
+        boolean isNumber(byte c) {
+            return 47 < c && c < 58;
+        }
+
+        boolean isAlphabet(byte c) {
+            return (64 < c && c < 91) || (96 < c && c < 123);
+        }
+
+        byte read() throws Exception {
+            if (index == size) {
+                size = System.in.read(buffer, index = 0, SIZE);
+                if (size < 0) buffer[0] = -1;
+            }
+            return buffer[index++];
+        }
     }
 }
