14a3d925d34ac3e546a948f0a4b0de84e21fb9d8
seong-wan <139252411+seong-wan@users.noreply.github.com>
2024-07-25 21:08:52 +0800

    [Gold IV] Title: 작업, Time: 424 ms, Memory: 39920 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.java
+++ 백준/Gold/2056. 작업/작업.java
@@ -15,7 +15,9 @@ public class Main {
     static PriorityQueue<Integer> pq = new PriorityQueue<>((e1, e2) -> time[e1] - time[e2]);
 
     public static void main(String[] args) throws Exception {
-        N = Integer.parseInt(br.readLine());
+        Reader in = new Reader();
+
+        N = in.nextInt();
 
         pre = new int[N];
         time = new int[N];
@@ -25,15 +27,14 @@ public class Main {
         }
 
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            time[i] = Integer.parseInt(st.nextToken());
+            time[i] = in.nextInt();
 
-            pre[i] = Integer.parseInt(st.nextToken());
+            pre[i] = in.nextInt();
             if (pre[i] == 0)
                 pq.add(i);
-            
+
             for (int j = 0; j < pre[i]; j++) {
-                int preNum = Integer.parseInt(st.nextToken()) - 1;
+                int preNum = in.nextInt() - 1;
                 postList[preNum].add(i);
             }
         }
@@ -55,5 +56,97 @@ public class Main {
         }
 
         System.out.println(ans);
+    }
+
+    static class Reader {
+        final int SIZE = 1 << 13;
+        byte[] buffer = new byte[SIZE];
+        int index, size;
+
+        String nextString() throws Exception {
+            StringBuilder sb = new StringBuilder();
+            byte c;
+            while ((c = read()) < 32) {
+                if (size < 0) return "endLine";
+            }
+            do sb.appendCodePoint(c);
+            while ((c = read()) > 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+            return sb.toString();
+        }
+
+        char nextChar() throws Exception {
+            byte c;
+            while ((c = read()) < 32) ; // SPACE 분리라면 <=로, SPACE 무시라면 <로
+            return (char) c;
+        }
+
+        int nextInt() throws Exception {
+            int n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) {
+                if (size < 0) return -1;
+            }
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        long nextLong() throws Exception {
+            long n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) ;
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        double nextDouble() throws Exception {
+            double n = 0, div = 1;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) {
+                if (size < 0) return -12345;
+            }
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            } else if (c == 46) {
+                c = read();
+            }
+            do n = (n * 10) + (c & 15);
+            while (isNumber(c = read()));
+            if (c == 46) {
+                while (isNumber(c = read())) {
+                    n += (c - 48) / (div *= 10);
+                }
+            }
+            return isMinus ? -n : n;
+        }
+
+        boolean isNumber(byte c) {
+            return 47 < c && c < 58;
+        }
+
+        boolean isAlphabet(byte c) {
+            return (64 < c && c < 91) || (96 < c && c < 123);
+        }
+
+        byte read() throws Exception {
+            if (index == size) {
+                size = System.in.read(buffer, index = 0, SIZE);
+                if (size < 0) buffer[0] = -1;
+            }
+            return buffer[index++];
+        }
     }
 }
