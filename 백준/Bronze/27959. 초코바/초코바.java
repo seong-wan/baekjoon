3fffe86ec75976a61004e9b8620381535211034d
seong-wan <139252411+seong-wan@users.noreply.github.com>
2024-07-30 02:22:09 +0800

    [Bronze V] Title: 초코바, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, M;
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        System.out.println(N * 100 >= M ? "Yes" : "No");
+    }
+}
