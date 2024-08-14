08cb87d9ec2849349d352e15ae7c5cd35933cf72
seong-wan <139252411+seong-wan@users.noreply.github.com>
2024-08-15 04:50:13 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10444 ms, Memory: 305668 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
@@ -7,10 +9,11 @@ public class Main {
     static List<Integer> hackingList;
     static int maxCount;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int M = scanner.nextInt();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         graph = new List[N + 1];
         for (int i = 1; i <= N; i++) {
@@ -18,9 +21,10 @@ public class Main {
         }
 
         for (int i = 0; i < M; i++) {
-            int A = scanner.nextInt();
-            int B = scanner.nextInt();
-            graph[B].add(A); 
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            graph[B].add(A);
         }
 
         hackingList = new ArrayList<>();
@@ -36,7 +40,7 @@ public class Main {
             sb.append(num).append(" ");
         }
 
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 
     private static void bfs(int start) {
