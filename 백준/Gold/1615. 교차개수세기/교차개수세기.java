75486bc7a67b8045428b3306f0ac26c27abe69ca
seong-wan <139252411+seong-wan@users.noreply.github.com>
2024-09-11 02:41:20 +0900

    [Gold I] Title: 교차개수세기, Time: 1168 ms, Memory: 35180 KB -BaekjoonHub

--- 백준/Gold/1615. 교차개수세기/교차개수세기.java
+++ 백준/Gold/1615. 교차개수세기/교차개수세기.java
@@ -1,56 +1,40 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 //세그먼트 트리를 이용하여 1열을 정렬 후 돌면서
 //연결되는 각 정점들의 인덱스 이후의 구간 합을 구하고 해당 인덱스에 1을 update 하는 식으로 진행
 //1열은 오름차순으로 정렬되어 있으므로 연결된 정점의 인덱스 다음으로 있는 간선이 있다면 그 개수만큼 교차개수 형성
 //1열의 인덱스가 같을 때는 2열의 인덱스를 오름차순으로 정렬하여 같은 정점에서 나온 간선으로 인해 교차점이 생기지 않게 처리
 //1열의 첫번째 인덱스는 교차할 간선이 없으므로 update만 진행
+//메모리 초과를 해결하기 위해 하나의 리스트에 a열과 b열의 값 두개를 a열의 값에 10000을 곱해서 한 개의 값으로 만듬
 public class Main {
-    static class Node implements Comparable<Node> {
-        int a;
-        int b;
-
-        @Override
-        public int compareTo(Node o) {
-            return this.a == o.a ? this.b - o.b : this.a - o.a;
-        }
-    }
-
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int[] tree;
-    static Node[] edge;
+    static int[] edge;
 
     public static void main(String[] args) throws Exception {
-        Reader in=new Reader();
+        Reader in = new Reader();
+
         int N = in.nextInt();
         int M = in.nextInt();
         long ans = 0;
         tree = new int[4 * N];
 
         //간선 입력
-        edge = new Node[M];
-
+        edge = new int[M];
         for (int i = 0; i < M; i++) {
             int a = in.nextInt();
             int b = in.nextInt();
-
-            edge[i] = new Node();
-            edge[i].a = a;
-            edge[i].b = b;
+            edge[i] = a * 10000 + b;
         }
 
         Arrays.sort(edge);
 
         for (int i = 0; i < M; i++) {
-            Node temp = edge[i];
-            if (temp.a != 1) {
-                ans += query(1, 1, N, temp.b + 1);
+            int a = edge[i] / 10000;
+            int b = edge[i] % 10000;
+            if (a != 1) {
+                ans += query(1, 1, N, b + 1);
             }
-
-            update(1, 1, N, temp.b);
+            update(1, 1, N, b);
         }
         System.out.println(ans);
     }
@@ -88,7 +72,6 @@ public class Main {
 
         return lR + RR;
     }
-
 
     static class Reader {
         final int SIZE = 1 << 13;
