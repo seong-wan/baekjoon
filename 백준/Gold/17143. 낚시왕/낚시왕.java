f2caaabeddb7fdf13c0678a7cda23ca23fb91fa7
seong-wan <139252411+seong-wan@users.noreply.github.com>
2025-03-24 16:26:01 +0900

    [Gold I] Title: 낚시왕, Time: 156 ms, Memory: 20204 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.java
+++ 백준/Gold/17143. 낚시왕/낚시왕.java
@@ -1,157 +1,219 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
-
-//상어가 겹칠 수 있으므로 2차원 배열만으로는 상어를 저장할 수 없음
-//상어의 움직임이 규칙적이므로 수학적으로 계산해놓기
-//상어를 ArrayList에 넣어놓고 map과 상호작용
 public class Main {
-	static int R,C,M,sum; 
-	static Shark[][] map;
-	static List<Shark> list = new ArrayList<>();
-	
-	
-	//d가 1 위, 2아래, 3 우, 4 좌
-	static int[] dy = {-1,1,0,0};
-	static int[] dx = {0,0,1,-1};
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		map = new Shark[R+1][C+1]; //0:dummy
-		
-		for(int i=0; i<M; i++) { //상어 수 만큼
-			st = new StringTokenizer(br.readLine());
-			int r = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			int s = Integer.parseInt(st.nextToken());
-			int d = Integer.parseInt(st.nextToken());
-			int z = Integer.parseInt(st.nextToken());
-			
-			Shark shark = new Shark(r,c,s,d-1,z); //delta 위치 보정 1234 -> 0123
-			
-			//list와 map에 넣어주기
-			list.add(shark);
-			map[r][c] = shark;
-			
-		}
-		
-		//풀이
-		for(int i=1; i<=C; i++) { //col 을 이동하면서 
-			//상어를 잡는다.
-			catchShark(i);
-			
-			//상어를 이동시킨다.
-			//map에는 변화가 적용되지 않았다.
-			moveShark();
-			
-			//상어를 정리한다.
-			//리스트를 이용하여 map에 적용해준다.
-			killShark();
-		}
-		
-		System.out.println(sum);
-	}
-	
-	//map에서 상어를 잡는다. 
-	static void catchShark(int col) { //현재 column 번호를 받아서 아래로 이동하면서
-		for(int i=1; i<=R; i++) { //밑으로 이동하면서
-			if(map[i][col] != null) { //상어가 있다면
-				sum += map[i][col].z; 
-				list.remove(map[i][col]); //잡은 상어 리스트에서 지우기
-				break; //한 마리 잡으면 끝남
-			}
-		}
-	}
-	
-	//list에서 상어를 움직인다.
-	//map에는 변화가 적용되 않았다.
-	static void moveShark() {
-		for (Shark shark : list) {
-			int r = shark.r;
-			int c = shark.c;
-			int s = shark.s;
-			int d = shark.d;
-			
-			switch(d) {
-				//상,하
-				case 0:
-				case 1:
-					s = s % ((R-1)*2); //제자리로 오는 계산을 수학적으로 선처리 후 남는 부분만 이동
-					for(int i=0; i<s; i++) {
-						//양끝이면 방향 전환
-						if(r == 1) d=1; // 위에 도착하면 방향을 상 -> 하
-						else if(r == R) d = 0; //아래에 도착하면 방향을 하->상
-						
-						r+= dy[d];
-					}
-					shark.r = r;
-					shark.d = d;
-					break;
-			
-					//좌,우
-				case 2:
-				case 3:
-					s = s % ((C-1)*2); //제자리로 오는 계산을 수학적으로 선처리 후 남는 부분만 이동
-					for(int i=0; i<s; i++) {
-						//양끝이면 방향 전환
-						if(c == 1) d=2; // 왼쪽에 도착하면 방향을 좌 -> 우
-						else if(c == C) d = 3; //오른쪽에 도착하면 방향을 우->좌
-						
-						c+= dx[d];
-					}
-					shark.c = c;
-					shark.d = d;
-					break;
-			}
-		}
-	}
-	
-	//기존 맵을 초기화
-	//list -> map 기록하면서 겹치면 잡아먹는 처리~~
-	static void killShark() {
-		//맵 초기화 
-		for(int i=1; i<=R; i++) {
-			for(int j=1; j<=C; j++) {
-				map[i][j] = null;
-			}
-		}
-		
-		//list를 이용해서 map 정리
-		int size = list.size();
-		
-		for(int i=size-1; i>=0; i--) { //삭제되기 때문에 뒤에서 부터 시작하기! index
-			Shark s = list.get(i);
-			
-			if(map[s.r][s.c] == null) {
-				map[s.r][s.c] = s;
-			}else {
-				if(s.z > map[s.r][s.c].z) { //list에서 꺼낸 상어가 더 크다면
-					list.remove(map[s.r][s.c]); //작은 상어를 list에서 지워주고
-					map[s.r][s.c] = s; //꺼낸 상어를 맵에 넣어주기
-				}else {//list에서 꺼낸 상어가 더 작다면
-					list.remove(i); //list에 있던 상어 지워주기
-				}
-			}
-		}
-	}
-	
-	//(r,c)위치, s 속력, d 이동방향, z 크기
-	static class Shark{
-		int r, c, s, d, z;
-		Shark(int r, int c, int s, int d, int z) {
+	static class shark {
+		//위치
+		int r;
+		int c;
+		//속력
+		int s;
+		//이동 방향
+		int d;
+		//크기
+		int z;
+
+		public shark(int r, int c, int s, int d, int z) {
 			this.r = r;
 			this.c = c;
 			this.s = s;
 			this.d = d;
 			this.z = z;
+		}
+	}
+
+	static int R, C, M, ans, fishManLoc;
+	static shark[] sharks;
+
+	static int[][] map;
+
+	public static void main(String[] args) throws Exception {
+		Reader in = new Reader();
+		R = in.nextInt();
+		C = in.nextInt();
+		M = in.nextInt();
+
+		sharks = new shark[M + 1];
+		for (int i = 1; i <= M; i++) {
+			sharks[i] = new shark(in.nextInt(), in.nextInt(), in.nextInt(), in.nextInt(), in.nextInt());
+		}
+
+		simul();
+
+		System.out.println(ans);
+	}
+
+	static void simul() {
+		while (true) {
+			//상어들의 현 위치를 맵에 표시
+			marking();
+
+			//낚시왕의 포획 시간
+			fishing();
+
+			//낚시 끝
+			if (fishManLoc == C)
+				return;
+
+			//상어들의 이동 시간
+			for (int i = 1; i <= M; i++) {
+				//이미 사라져버린 불쌍한 상어들..
+				if (sharks[i] == null)
+					continue;
+
+				int dir = sharks[i].d;
+
+				//위로 향하는 상어의 경우 - r만 움직임
+				if (dir == 1) {
+					//가장 아래에서 부터의 거리
+					int distance = R - sharks[i].r;
+					int movePoint = distance + sharks[i].s;
+
+					int reverseCount = movePoint / (R - 1);
+					int afterDistance = movePoint % (R - 1);
+
+					//방향이 안 바뀐 경우
+					if (reverseCount % 2 == 0) {
+						sharks[i].r = R - afterDistance;
+					}
+					//방향이 바뀐 경우
+					else {
+						sharks[i].d = 2;
+						sharks[i].r = 1 + afterDistance;
+					}
+				}
+
+				//아래로 향하는 상어의 경우 - r만 움직임
+				else if (dir == 2) {
+					//가장 아래에서 부터의 거리
+					int distance = sharks[i].r - 1;
+					int movePoint = distance + sharks[i].s;
+
+					int reverseCount = movePoint / (R - 1);
+					int afterDistance = movePoint % (R - 1);
+
+					//방향이 안 바뀐 경우
+					if (reverseCount % 2 == 0) {
+						sharks[i].r = 1 + afterDistance;
+					}
+					//방향이 바뀐 경우
+					else {
+						sharks[i].d = 1;
+						sharks[i].r = R - afterDistance;
+					}
+				}
+
+				//오른쪽으로 향하는 상어의 경우 - c만 움직임
+				else if (dir == 3) {
+					//가장 아래에서 부터의 거리
+					int distance = sharks[i].c - 1;
+					int movePoint = distance + sharks[i].s;
+
+					int reverseCount = movePoint / (C - 1);
+					int afterDistance = movePoint % (C - 1);
+
+					//방향이 안 바뀐 경우
+					if (reverseCount % 2 == 0) {
+						sharks[i].c = 1 + afterDistance;
+					}
+					//방향이 바뀐 경우
+					else {
+						sharks[i].d = 4;
+						sharks[i].c = C - afterDistance;
+					}
+				}
+
+				//왼쪽으로 향하는 상어의 경우 - c만 움직임
+				else if (dir == 4) {
+					//가장 아래에서 부터의 거리
+					int distance = C - sharks[i].c;
+					int movePoint = distance + sharks[i].s;
+
+					int reverseCount = movePoint / (C - 1);
+					int afterDistance = movePoint % (C - 1);
+
+					//방향이 안 바뀐 경우
+					if (reverseCount % 2 == 0) {
+						sharks[i].c = C - afterDistance;
+					}
+					//방향이 바뀐 경우
+					else {
+						sharks[i].d = 3;
+						sharks[i].c = 1 + afterDistance;
+					}
+				}
+			}
+		}
+	}
+
+	static void marking() {
+		map = new int[R + 1][C + 1];
+
+		for (int i = 1; i <= M; i++) {
+			if (sharks[i] == null)
+				continue;
+
+			//이미 다른 상어가 있는 경우
+			if (map[sharks[i].r][sharks[i].c] != 0) {
+				int originSharkSize = sharks[map[sharks[i].r][sharks[i].c]].z;
+				int newSharkSize = sharks[i].z;
+
+				if (originSharkSize > newSharkSize) {
+					sharks[i] = null;
+					continue;
+				} else {
+					sharks[map[sharks[i].r][sharks[i].c]] = null;
+				}
+			}
+
+			map[sharks[i].r][sharks[i].c] = i;
+		}
+	}
+
+	static void fishing() {
+		fishManLoc++;
+		for (int i = 1; i <= R; i++) {
+			//땅에 가장 가까운 위치에 있는 상어 포획
+			if (map[i][fishManLoc] != 0) {
+				int sharkNum = map[i][fishManLoc];
+				ans += sharks[sharkNum].z;
+				sharks[sharkNum] = null;
+				break;
+			}
+		}
+	}
+
+	static class Reader {
+		final int SIZE = 1 << 13;
+		byte[] buffer = new byte[SIZE];
+		int index, size;
+
+		int nextInt() throws Exception {
+			int n = 0;
+			byte c;
+			boolean isMinus = false;
+			while ((c = read()) <= 32) {
+				if (size < 0)
+					return -1;
+			}
+			if (c == 45) {
+				c = read();
+				isMinus = true;
+			}
+			do
+				n = (n << 3) + (n << 1) + (c & 15);
+			while (isNumber(c = read()));
+			return isMinus ? ~n + 1 : n;
+		}
+
+		boolean isNumber(byte c) {
+			return 47 < c && c < 58;
+		}
+
+		byte read() throws Exception {
+			if (index == size) {
+				size = System.in.read(buffer, index = 0, SIZE);
+				if (size < 0)
+					buffer[0] = -1;
+			}
+			return buffer[index++];
 		}
 	}
 }
