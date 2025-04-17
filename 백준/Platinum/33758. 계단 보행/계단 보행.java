2d76113f05416d926bc4a00dd1cda673fea87d59
seong-wan <139252411+seong-wan@users.noreply.github.com>
2025-04-17 10:38:05 +0900

    [Platinum IV] Title: 계단 보행, Time: 200 ms, Memory: 45896 KB, Score: 11 point -BaekjoonHub

---
+++ 백준/Platinum/33758. 계단 보행/계단 보행.java
@@ -0,0 +1,120 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+	static int N, M;
+	static List<int[]>[] adlist;
+	static Set<Integer>[] visit;
+	static int[] memoi;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		Reader in = new Reader();
+
+		N = in.nextInt();
+		M = in.nextInt();
+
+		memoi = new int[N + 1];
+		adlist = new List[N + 1];
+		visit = new Set[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			adlist[i] = new ArrayList<>();
+			visit[i] = new HashSet<>();
+		}
+
+		for (int i = 0; i < M; i++) {
+			int from = in.nextInt();
+			int to = in.nextInt();
+			int num = in.nextInt();
+
+			adlist[from].add(new int[] {to, num, i});
+			adlist[to].add(new int[] {from, num, i});
+		}
+
+		bfs();
+
+		for (int i = 1; i <= N; i++) {
+			sb.append(memoi[i] == 0 ? -1 : memoi[i]).append(" ");
+		}
+
+		System.out.println(sb);
+	}
+
+	static void bfs() {
+		Deque<int[]> queue = new ArrayDeque<>();
+
+		for (int[] next : adlist[1]) {
+			visit[next[0]].add(next[2]);
+			queue.add(new int[] {next[0], next[1]});
+			memoi[next[0]] = 1;
+		}
+
+		int depth = 2;
+
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				int[] cur = queue.poll();
+				int node = cur[0];
+				int num = cur[1];
+
+				for (int[] next : adlist[node]) {
+					if (visit[next[0]].contains(next[2]))
+						continue;
+
+					if (Math.abs(num - next[1]) != 1)
+						continue;
+
+					if (memoi[next[0]] == 0)
+						memoi[next[0]] = depth;
+
+					visit[next[0]].add(next[2]);
+					queue.add(new int[] {next[0], next[1]});
+				}
+			}
+			depth++;
+		}
+	}
+
+	static class Reader {
+		final int SIZE = 1 << 13;
+		byte[] buffer = new byte[SIZE];
+		int index, size;
+
+		int nextInt() throws Exception {
+			int n = 0;
+			byte c;
+			boolean isMinus = false;
+			while ((c = read()) <= 32) {
+				if (size < 0)
+					return -1;
+			}
+			if (c == 45) {
+				c = read();
+				isMinus = true;
+			}
+			do
+				n = (n << 3) + (n << 1) + (c & 15);
+			while (isNumber(c = read()));
+			return isMinus ? ~n + 1 : n;
+		}
+
+		boolean isNumber(byte c) {
+			return 47 < c && c < 58;
+		}
+
+		byte read() throws Exception {
+			if (index == size) {
+				size = System.in.read(buffer, index = 0, SIZE);
+				if (size < 0)
+					buffer[0] = -1;
+			}
+			return buffer[index++];
+		}
+	}
+}
