377fc85171b6e807c602f1adaf247c450ef3d6c8
seong-wan <139252411+seong-wan@users.noreply.github.com>
2025-06-11 06:23:05 +0900

    [Silver II] Title: 타일링, Time: 120 ms, Memory: 15000 KB -BaekjoonHub

--- 백준/Silver/1793. 타일링/타일링.java
+++ 백준/Silver/1793. 타일링/타일링.java
@@ -1,26 +1,31 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
 
 public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int N;
+	static BigInteger[] dp = new BigInteger[251];
+	static StringBuilder sb = new StringBuilder();
 
-	static BigInteger[] dp;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
-		dp = new BigInteger[251];
-		
-		dp[0] =new BigInteger("1");
-		dp[1] =new BigInteger("1");
-		dp[2] =new BigInteger("3");
-		
-		for(int i=3; i<251; i++) {
-			dp[i] = dp[i-1].add(dp[i-2].multiply(new BigInteger("2")));
+	public static void main(String[] args) throws Exception {
+		init();
+
+		String input = "";
+		while ((input = br.readLine()) != null && !input.isEmpty()) {
+			sb.append(dp[Integer.parseInt(input)]).append("\n");
 		}
 
-		String line = null;
-		while((line=br.readLine())!=null) {
-			int n = Integer.parseInt(line);
-			System.out.println(dp[n]);
+		System.out.println(sb);
+	}
+
+	static void init() {
+
+		dp[0] = new BigInteger("1");
+		dp[1] = new BigInteger("1");
+
+		for (int i = 2; i <= 250; i++) {
+			dp[i] = dp[i - 1].add(dp[i - 2].multiply(new BigInteger("2")));
 		}
-		br.close();
 	}
 }
