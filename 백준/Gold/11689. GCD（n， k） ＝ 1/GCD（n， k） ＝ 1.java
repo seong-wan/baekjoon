41bf1dc5c050a17e6d77edbf96b1dbb165db8e2a
seong-wan <139252411+seong-wan@users.noreply.github.com>
2025-07-06 10:46:08 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 116 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -1,54 +1,33 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static long N, ans = 1;
+	static long N, ans;
 	static int maxVal;
-	static boolean[] notP;
-	static HashMap<Integer, Integer> primeFacto = new HashMap<>();
 
 	public static void main(String[] args) throws Exception {
 		N = Long.parseLong(br.readLine());
+		ans = N;
 		maxVal = (int)Math.sqrt(N);
 
-		notP = new boolean[maxVal + 1];
-		calcP();
-
 		for (int i = 2; i <= maxVal; i++) {
-			if (notP[i])
+			if (N == 1)
+				break;
+
+			if (N % i != 0)
 				continue;
+
+			ans -= ans / i;
 
 			while (N % i == 0) {
 				N /= i;
-				primeFacto.putIfAbsent(i, 0);
-				primeFacto.put(i, primeFacto.get(i) + 1);
 			}
 		}
 
 		if (N != 1)
-			ans = N - 1;
-
-		for (Integer p : primeFacto.keySet()) {
-			int exp = primeFacto.get(p);
-			long oilVal = (long)(Math.pow(p, exp) - Math.pow(p, exp - 1));
-
-			ans *= oilVal;
-		}
+			ans -= ans / N;
 
 		System.out.print(ans);
-	}
-
-	static void calcP() {
-		notP[0] = notP[1] = true;
-		for (int i = 2; i <= maxVal; i++) {
-			if (notP[i])
-				continue;
-
-			for (int j = i * 2; j <= maxVal; j += i) {
-				notP[j] = true;
-			}
-		}
 	}
 }
