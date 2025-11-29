c3fec68eb04b17839093dbd53c84f94106558b8d
seong-wan <139252411+seong-wan@users.noreply.github.com>
2025-11-30 04:30:12 +0900

    [Silver III] Title: 두 수의 합, Time: 140 ms, Memory: 18568 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,67 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+	static int n, x, ans;
+	static int[] input = new int[100000];
+	static Set<Integer> set = new HashSet<>();
+
+	public static void main(String[] args) throws Exception {
+		Reader in = new Reader();
+		n = in.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			input[i] = in.nextInt();
+		}
+		x = in.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			if (input[i] >= x)
+				continue;
+
+			if (set.contains(input[i]))
+				ans++;
+			else
+				set.add(x - input[i]);
+		}
+
+		System.out.print(ans);
+	}
+
+	static class Reader {
+		final int SIZE = 1 << 13;
+		byte[] buffer = new byte[SIZE];
+		int index, size;
+
+		int nextInt() throws Exception {
+			int n = 0;
+			byte c;
+			boolean isMinus = false;
+			while ((c = read()) <= 32) {
+				if (size < 0)
+					return -1;
+			}
+			if (c == 45) {
+				c = read();
+				isMinus = true;
+			}
+			do
+				n = (n << 3) + (n << 1) + (c & 15);
+			while (isNumber(c = read()));
+			return isMinus ? ~n + 1 : n;
+		}
+
+		boolean isNumber(byte c) {
+			return 47 < c && c < 58;
+		}
+
+		byte read() throws Exception {
+			if (index == size) {
+				size = System.in.read(buffer, index = 0, SIZE);
+				if (size < 0)
+					buffer[0] = -1;
+			}
+			return buffer[index++];
+		}
+	}
+}
