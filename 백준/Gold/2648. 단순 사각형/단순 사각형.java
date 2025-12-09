ec3e5de73fbd18031b2374d05e12613c3939e48c
seong-wan <139252411+seong-wan@users.noreply.github.com>
2025-12-10 03:45:25 +0900

    [Gold III] Title: 단순 사각형, Time: 252 ms, Memory: 17512 KB -BaekjoonHub

--- 백준/Gold/2648. 단순 사각형/단순 사각형.java
+++ 백준/Gold/2648. 단순 사각형/단순 사각형.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -20,34 +18,25 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static Point loc;
-	static boolean[][] map = new boolean[101][101];
 	static int n, minSize = Integer.MAX_VALUE;
 	static int[] dy = {1, -1, 0, 0};
 	static int[] dx = {0, 0, 1, -1};
 	static List<Point> candidates = new ArrayList<>();
 	static Point[] minPoints = new Point[2];
-	static Set<Integer>[][] connected = new Set[101][101];
+	static int[][][] connected = new int[101][101][2];
 
 	public static void main(String[] args) throws Exception {
-		for (int i = 1; i <= 100; i++) {
-			for (int j = 1; j <= 100; j++) {
-				connected[i][j] = new HashSet<>();
-			}
-		}
-
 		st = new StringTokenizer(br.readLine());
 		loc = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 
 		n = Integer.parseInt(br.readLine());
 
-		map[loc.x][loc.y] = true;
-		int lastDir = -1;
-
-		for (int i = 0; i < n; i++) {
+		for (int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
 
 			String direction = st.nextToken();
 			int dir = 0;
+			int vertical = 1;
 			switch (direction) {
 				case "U": {
 					break;
@@ -58,34 +47,30 @@ public class Main {
 				}
 				case "R": {
 					dir = 2;
+					vertical = 0;
 					break;
 				}
 				case "L": {
 					dir = 3;
+					vertical = 0;
 				}
 			}
 
 			int dis = Integer.parseInt(st.nextToken());
-
-			//코너가 되는 상황
-			if (lastDir != -1 && dir != lastDir) {
-				candidates.add(new Point(loc.x, loc.y));
-			}
-
-			lastDir = dir;
-			connected[loc.x][loc.y].add(i);
+			connected[loc.x][loc.y][vertical] = i;
 
 			for (int j = 0; j < dis; j++) {
 				loc.x += dx[dir];
 				loc.y += dy[dir];
 
 				//기존에 선분이 있는 상황(수직*수평)
-				if (map[loc.x][loc.y])
+				if (connected[loc.x][loc.y][vertical == 1 ? 0 : 1] != 0)
 					candidates.add(new Point(loc.x, loc.y));
 
-				map[loc.x][loc.y] = true;
-				connected[loc.x][loc.y].add(i);
+				connected[loc.x][loc.y][vertical] = i;
 			}
+
+			candidates.add(new Point(loc.x, loc.y));
 		}
 
 		candidates.sort((e1, e2) -> e1.y == e2.y ? e1.x - e2.x : e1.y - e2.y);
@@ -102,7 +87,7 @@ public class Main {
 
 						//사각형 조건 만족하는지 확인
 						if (p1.y == p2.y && p3.y == p4.y && p1.x == p3.x && p2.x == p4.x) {
-							if (!check(p1, p2, p3) || !check(p4, p2, p3))
+							if (!check(p1, p2, p3) || !check(p4, p3, p2))
 								continue;
 
 							int squareSize = (p2.x - p1.x) * (p3.y - p1.y);
@@ -127,17 +112,12 @@ public class Main {
 	}
 
 	static boolean check(Point p1, Point p2, Point p3) {
-		boolean connectA = false;
-		boolean connectB = false;
+		if (connected[p1.x][p1.y][0] != connected[p2.x][p2.y][0])
+			return false;
 
-		for (Integer i : connected[p1.x][p1.y]) {
-			if (connected[p2.x][p2.y].contains(i))
-				connectA = true;
+		if (connected[p1.x][p1.y][1] != connected[p3.x][p3.y][1])
+			return false;
 
-			if (connected[p3.x][p3.y].contains(i))
-				connectB = true;
-		}
-
-		return connectA && connectB;
+		return true;
 	}
 }
