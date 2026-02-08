3adb5c46dabd675be977f0e1836592876f3e30d5
seong-wan <139252411+seong-wan@users.noreply.github.com>
2026-02-09 00:29:24 +0900

    [Gold IV] Title: 미니 버킷 리스트, Time: 132 ms, Memory: 14832 KB -BaekjoonHub

---
+++ 백준/Gold/24270. 미니 버킷 리스트/미니 버킷 리스트.java
@@ -0,0 +1,61 @@
+public class Main {
+	static int N, K, sum;
+	static final int div = 1_000_000_007;
+	static long ans = 1;
+
+	public static void main(String[] args) throws Exception {
+		Reader in = new Reader();
+		N = in.nextInt();
+		K = in.nextInt();
+
+		for (int i = 0; i < N; i++) {
+			sum += in.nextInt();
+		}
+
+		int base = K - sum;
+
+		for (int i = 1; i <= N; i++) {
+			ans *= base + i;
+			ans %= div;
+		}
+
+		System.out.print(ans);
+	}
+
+	static class Reader {
+		final int SIZE = 1 << 13;
+		byte[] buffer = new byte[SIZE];
+		int index, size;
+
+		int nextInt() throws Exception {
+			int n = 0;
+			byte c;
+			boolean isMinus = false;
+			while ((c = read()) <= 32) {
+				if (size < 0)
+					return -1;
+			}
+			if (c == 45) {
+				c = read();
+				isMinus = true;
+			}
+			do
+				n = (n << 3) + (n << 1) + (c & 15);
+			while (isNumber(c = read()));
+			return isMinus ? ~n + 1 : n;
+		}
+
+		boolean isNumber(byte c) {
+			return 47 < c && c < 58;
+		}
+
+		byte read() throws Exception {
+			if (index == size) {
+				size = System.in.read(buffer, index = 0, SIZE);
+				if (size < 0)
+					buffer[0] = -1;
+			}
+			return buffer[index++];
+		}
+	}
+}
