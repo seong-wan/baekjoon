2b04e2f27f915c81e417fe8b3045148b088d1cd2
seong-wan <139252411+seong-wan@users.noreply.github.com>
2026-03-14 02:29:40 +0900

    [Silver V] Title: 중복된 숫자, Time: 1440 ms, Memory: 507320 KB -BaekjoonHub

--- 백준/Silver/15719. 중복된 숫자/중복된 숫자.java
+++ 백준/Silver/15719. 중복된 숫자/중복된 숫자.java
@@ -4,23 +4,19 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static boolean[] check;
+	static int N;
+	static long sum;
 
 	public static void main(String[] args) throws Exception {
-		int N = Integer.parseInt(br.readLine());
-		check = new boolean[N];
+		N = Integer.parseInt(br.readLine());
+
+		long expect = (long)N * (N - 1) / 2;
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		for (int i = 0; i < N; i++) {
-			int temp = Integer.parseInt(st.nextToken());
-
-			if (check[temp]) {
-				System.out.print(temp);
-				return;
-			}
-
-			check[temp] = true;
+			sum += Long.parseLong(st.nextToken());
 		}
+
+		System.out.println(sum - expect);
 	}
 }
