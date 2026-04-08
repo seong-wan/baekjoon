9f79b0f13916b7791d3a25a249315e5c71f20626
seong-wan <139252411+seong-wan@users.noreply.github.com>
2026-04-09 00:50:43 +0900

    [Gold V] Title: 전생했더니 슬라임 연구자였던 건에 대하여 (Easy), Time: 116 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Gold/14715. 전생했더니 슬라임 연구자였던 건에 대하여 （Easy）/전생했더니 슬라임 연구자였던 건에 대하여 （Easy）.java
+++ 백준/Gold/14715. 전생했더니 슬라임 연구자였던 건에 대하여 （Easy）/전생했더니 슬라임 연구자였던 건에 대하여 （Easy）.java
@@ -4,11 +4,8 @@ import java.io.InputStreamReader;
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static int N, cnt;
-	static boolean[] isNotPrime = new boolean[1000001];
 
 	public static void main(String[] args) throws Exception {
-		init();
-
 		N = Integer.parseInt(br.readLine());
 
 		calc();
@@ -18,9 +15,6 @@ public class Main {
 
 	static void calc() {
 		for (int i = 2; i <= 1000000; i++) {
-			if (isNotPrime[i])
-				continue;
-
 			while (N % i == 0) {
 				cnt++;
 				N /= i;
@@ -28,18 +22,6 @@ public class Main {
 
 			if (N == 1)
 				break;
-		}
-	}
-
-	static void init() {
-		isNotPrime[0] = isNotPrime[1] = true;
-
-		for (int i = 2; i <= Math.sqrt(1000000); i++) {
-			if (isNotPrime[i])
-				continue;
-
-			for (int j = i * 2; j <= 1000000; j += i)
-				isNotPrime[j] = true;
 		}
 	}
 }
